b7071deee5c025c56bc41686e189f880db4012af
Harrison <harrison.bradley.turpin@gmail.com>
2024-10-17 18:53:02 +0100

    Reorganise

---
+++ gl_additions/mod.cpp
@@ -0,0 +1,12 @@
+name = "Grenade Launcher Additions";
+picture = "mod.paa";
+actionName = "";
+action = "";
+description = "GL Additions";
+logo = "mod.paa";
+logoOver = "mod.paa";
+tooltip = "GL Additions";
+tooltipOwned = "";
+overview = "GL Additions";
+author = "HBTurpin";
+
