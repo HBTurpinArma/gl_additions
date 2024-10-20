17ac90b91a101ad123dee35395632abddef62d2d
Harrison <harrison.bradley.turpin@gmail.com>
2024-10-20 12:01:39 +0100

    MagazineWells Setup

---
+++ gl_additions/addons/GLA/CfgMagazineWells.hpp
@@ -0,0 +1,42 @@
+//Use the below to add the ammo types to the CBA compatable ammo types used in a majority of guns for compatability reasons.
+//In future we may want to look at adding other GL alternatives.
+class CfgMagazineWells
+{
+	class CBA_40mm_M203
+	{
+		GLA_Ammo[]=
+		{
+			"gla_mag_m4009_impact",
+			"gla_mag_M651_CS",
+			"gla_mag_M1060_Thermobaric",
+			"gla_mag_APERS40_MINE",
+			"gla_mag_Drone40_Recon"
+		};
+	};
+	class CBA_40mm_3GL
+	{
+		GLA_Ammo[]=
+		{
+			"gla_mag_3Rnd_M441_HE",
+			"gla_mag_3Rnd_M397_HET",
+			"gla_mag_3Rnd_m4009",
+			"gla_mag_3Rnd_m4009_impact",
+			"gla_mag_3Rnd_M433_HEDP",
+			"gla_mag_3Rnd_M576_Buckshot",
+			"gla_mag_3Rnd_M781_Practice",
+			"gla_mag_3Rnd_M651_CS",
+			"gla_mag_3Rnd_M1060_Thermobaric",
+			"gla_mag_3Rnd_APERS40_MINE"
+		};
+	};
+	class CBA_40mm_M203_6rnds
+	{
+		GLA_Ammo[]=
+		{
+			"gla_mag_6Rnd_m4009_impact",
+			"gla_mag_6Rnd_M651_CS",
+			"gla_mag_6Rnd_M1060_Thermobaric",
+			"gla_mag_6Rnd_APERS40_MINE"
+		};
+	};
+};
