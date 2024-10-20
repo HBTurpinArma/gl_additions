a6c68f1915e9b7a96d94344010923f832bd408a5
Harrison <harrison.bradley.turpin@gmail.com>
2024-10-20 12:01:51 +0100

    Versioning Vars

--- gl_additions/addons/GLA/config_macros.hpp
+++ gl_additions/addons/GLA/config_macros.hpp
@@ -3,3 +3,9 @@
 #define FUNC(name) ADDON##_fnc_##name
 #define QUOTE(target) #target
 #define FUNCTION_PATH ADDON##\scripts
+
+
+#define MAJOR 2
+#define MINOR 1
+#define PATCHLVL 1
+#define BUILD 1
